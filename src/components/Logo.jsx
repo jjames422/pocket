178bbf05a82fe99d91cf293144a1f7e280ff8d1b
Jonathan <jjames422@me.com>
2023-07-03 16:00:37 -0400

    svg updated

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -13,355 +13,100 @@ export function Logomark(props) {
 
 export function Logo(props) {
   return(
-  <svg
-    id="Layer_1"
+    <svg
     xmlns="http://www.w3.org/2000/svg"
-    xmlnsXlink="http://www.w3.org/1999/xlink"
-    x={0}
-    y={0}
-    viewBox="0 0 1080 1080"
-    style={{
-      enableBackground: "new 0 0 1080 1080",
-    }}
-    xmlSpace="preserve"
+    data-name="Layer 1"
+    viewBox="0 0 792.7 971.4"
     {...props}
   >
-    <style>
-      {
-        ".st1{clip-path:url(#SVGID_00000151519134926826946120000005034476641655925421_)}.st2{fill:url(#SVGID_00000127006392037994694050000003452529175055724942_)}.st3{fill:url(#SVGID_00000003803997498163791590000016901666912849951110_)}"
-      }
-    </style>
-    <linearGradient
-      id="SVGID_1_"
-      gradientUnits="userSpaceOnUse"
-      x1={513.074}
-      y1={586.86}
-      x2={595.58}
-      y2={760.792}
-    >
-      <stop
-        offset={0}
-        style={{
-          stopColor: "#652c8f",
-        }}
-      />
-      <stop
-        offset={0.158}
-        style={{
-          stopColor: "#672f90",
-        }}
-      />
-      <stop
-        offset={0.261}
-        style={{
-          stopColor: "#6c3793",
-        }}
-      />
-      <stop
-        offset={0.348}
-        style={{
-          stopColor: "#764697",
-        }}
-      />
-      <stop
-        offset={0.426}
-        style={{
-          stopColor: "#845b9e",
-        }}
-      />
-      <stop
-        offset={0.498}
-        style={{
-          stopColor: "#9676a6",
-        }}
-      />
-      <stop
-        offset={0.5}
-        style={{
-          stopColor: "#9677a6",
-        }}
-      />
-      <stop
-        offset={0.515}
-        style={{
-          stopColor: "#937eaa",
-        }}
-      />
-      <stop
-        offset={0.589}
-        style={{
-          stopColor: "#869abc",
-        }}
-      />
-      <stop
-        offset={0.669}
-        style={{
-          stopColor: "#7cb0ca",
-        }}
-      />
-      <stop
-        offset={0.756}
-        style={{
-          stopColor: "#75bfd4",
-        }}
-      />
-      <stop
-        offset={0.856}
-        style={{
-          stopColor: "#70c8da",
-        }}
-      />
-      <stop
-        offset={1}
-        style={{
-          stopColor: "#6fcbdc",
-        }}
-      />
-    </linearGradient>
-    <circle
-      cx={546.5}
-      cy={657.3}
-      r={49.1}
-      style={{
-        fill: "url(#SVGID_1_)",
-      }}
-    />
     <defs>
-      <path
-        id="SVGID_00000019674923038653740110000013333303067014549632_"
-        d="M936.3 148.1c0-37.7-22.6-71.8-57.4-86.5-34.8-14.7-75-7-102 19.3L537 315.4 303.6 83.1C276.8 56.3 236.5 48.4 201.5 63c-35 14.5-57.8 48.7-57.8 86.6V653c0 19.9 6.3 38.4 16.8 53.6 40.3 180.5 191.7 319.1 366.4 319.1h27.6c175.8 0 328-140.3 367.2-322.4 9.2-14.5 14.7-31.7 14.7-50.2v-505zm-381.8 690h-27.6c-94.8 0-181.9-90.8-195.7-197.2V375.3l139 138.3c36.3 36.2 95 36.5 131.7.6l146.8-143.3v279.6c-17.6 102.2-102.2 187.6-194.2 187.6z"
-      />
+      <linearGradient
+        id="uuid-153606bb-c80f-4f34-af6c-307f968c667f"
+        x1="369.38"
+        x2="451.89"
+        y1="438.84"
+        y2="264.91"
+        gradientTransform="matrix(1 0 0 -1 0 971.4)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0" stopColor="#652c8f"></stop>
+        <stop offset="0.16" stopColor="#672f90"></stop>
+        <stop offset="0.26" stopColor="#6c3793"></stop>
+        <stop offset="0.35" stopColor="#764697"></stop>
+        <stop offset="0.43" stopColor="#845b9e"></stop>
+        <stop offset="0.5" stopColor="#9676a6"></stop>
+        <stop offset="0.5" stopColor="#9677a6"></stop>
+        <stop offset="0.52" stopColor="#937eaa"></stop>
+        <stop offset="0.59" stopColor="#869abc"></stop>
+        <stop offset="0.67" stopColor="#7cb0ca"></stop>
+        <stop offset="0.76" stopColor="#75bfd4"></stop>
+        <stop offset="0.86" stopColor="#70c8da"></stop>
+        <stop offset="1" stopColor="#6fcbdc"></stop>
+      </linearGradient>
+      <linearGradient
+        id="uuid-92cc01c7-3ab1-49ea-b490-1d0c82972238"
+        x1="628.99"
+        x2="215.72"
+        y1="190.36"
+        y2="410.38"
+        gradientTransform="matrix(1 0 0 -1 0 971.4)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0" stopColor="#652c8f"></stop>
+        <stop offset="0.16" stopColor="#672f90"></stop>
+        <stop offset="0.26" stopColor="#6c3793"></stop>
+        <stop offset="0.35" stopColor="#764697"></stop>
+        <stop offset="0.43" stopColor="#845b9e"></stop>
+        <stop offset="0.5" stopColor="#9676a6"></stop>
+        <stop offset="0.5" stopColor="#9677a6"></stop>
+        <stop offset="0.52" stopColor="#937eaa"></stop>
+        <stop offset="0.59" stopColor="#869abc"></stop>
+        <stop offset="0.67" stopColor="#7cb0ca"></stop>
+        <stop offset="0.76" stopColor="#75bfd4"></stop>
+        <stop offset="0.86" stopColor="#70c8da"></stop>
+        <stop offset="1" stopColor="#6fcbdc"></stop>
+      </linearGradient>
+      <linearGradient
+        id="uuid-5a6d7213-39e7-408a-85fa-4c7fbd27f654"
+        x1="201.29"
+        x2="675.27"
+        y1="861.71"
+        y2="286.87"
+        gradientTransform="matrix(1 0 0 -1 0 971.4)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0" stopColor="#652c8f"></stop>
+        <stop offset="0.16" stopColor="#672f90"></stop>
+        <stop offset="0.26" stopColor="#6c3793"></stop>
+        <stop offset="0.35" stopColor="#764697"></stop>
+        <stop offset="0.43" stopColor="#845b9e"></stop>
+        <stop offset="0.5" stopColor="#9676a6"></stop>
+        <stop offset="0.5" stopColor="#9677a6"></stop>
+        <stop offset="0.52" stopColor="#937eaa"></stop>
+        <stop offset="0.59" stopColor="#869abc"></stop>
+        <stop offset="0.67" stopColor="#7cb0ca"></stop>
+        <stop offset="0.76" stopColor="#75bfd4"></stop>
+        <stop offset="0.86" stopColor="#70c8da"></stop>
+        <stop offset="1" stopColor="#6fcbdc"></stop>
+      </linearGradient>
     </defs>
-    <clipPath id="SVGID_00000110462261458306570130000014292094130942276262_">
-      <use
-        xlinkHref="#SVGID_00000019674923038653740110000013333303067014549632_"
-        style={{
-          overflow: "visible",
-        }}
-      />
-    </clipPath>
-    <g
-      style={{
-        clipPath:
-          "url(#SVGID_00000110462261458306570130000014292094130942276262_)",
-      }}
-    >
-      <image
-        style={{
-          overflow: "visible",
-          opacity: 0.75,
-        }}
-        width={4127}
-        height={2986}
-        xlinkHref="DC5B90F84F5EF419.png"
-        transform="matrix(.24 0 0 .24 87.24 455.16)"
-      />
-      <linearGradient
-        id="SVGID_00000106121463363144746520000010712134911049142927_"
-        gradientUnits="userSpaceOnUse"
-        x1={772.707}
-        y1={835.309}
-        x2={359.433}
-        y2={615.293}
-      >
-        <stop
-          offset={0}
-          style={{
-            stopColor: "#652c8f",
-          }}
-        />
-        <stop
-          offset={0.158}
-          style={{
-            stopColor: "#672f90",
-          }}
-        />
-        <stop
-          offset={0.261}
-          style={{
-            stopColor: "#6c3793",
-          }}
-        />
-        <stop
-          offset={0.348}
-          style={{
-            stopColor: "#764697",
-          }}
-        />
-        <stop
-          offset={0.426}
-          style={{
-            stopColor: "#845b9e",
-          }}
-        />
-        <stop
-          offset={0.498}
-          style={{
-            stopColor: "#9676a6",
-          }}
-        />
-        <stop
-          offset={0.5}
-          style={{
-            stopColor: "#9677a6",
-          }}
-        />
-        <stop
-          offset={0.515}
-          style={{
-            stopColor: "#937eaa",
-          }}
-        />
-        <stop
-          offset={0.589}
-          style={{
-            stopColor: "#869abc",
-          }}
-        />
-        <stop
-          offset={0.669}
-          style={{
-            stopColor: "#7cb0ca",
-          }}
-        />
-        <stop
-          offset={0.756}
-          style={{
-            stopColor: "#75bfd4",
-          }}
-        />
-        <stop
-          offset={0.856}
-          style={{
-            stopColor: "#70c8da",
-          }}
-        />
-        <stop
-          offset={1}
-          style={{
-            stopColor: "#6fcbdc",
-          }}
-        />
-      </linearGradient>
-      <path
-        style={{
-          fill: "url(#SVGID_00000106121463363144746520000010712134911049142927_)",
-        }}
-        d="M554.5 1025.7h-27.6c-204.4 0-377-189.6-377-414.1 0-51.8 40.2-93.8 89.7-93.8s89.7 42 89.7 93.8c0 118.5 94.2 226.4 197.6 226.4h27.6c103.4 0 197.6-107.9 197.6-226.4 0-51.8 40.2-93.8 89.7-93.8s89.7 42 89.7 93.8c0 224.5-172.6 414.1-377 414.1z"
-      />
-    </g>
-    <g
-      style={{
-        clipPath:
-          "url(#SVGID_00000110462261458306570130000014292094130942276262_)",
-      }}
-    >
-      <image
-        style={{
-          overflow: "visible",
-          opacity: 0.55,
-        }}
-        width={4090}
-        height={3672}
-        xlinkHref="DC5B90F84F5EF41B.png"
-        transform="matrix(.24 0 0 .24 91.915 2.635)"
-      />
-      <linearGradient
-        id="SVGID_00000029030604317342557740000017856680528956840323_"
-        gradientUnits="userSpaceOnUse"
-        x1={344.945}
-        y1={164.027}
-        x2={818.931}
-        y2={738.861}
-      >
-        <stop
-          offset={0}
-          style={{
-            stopColor: "#652c8f",
-          }}
-        />
-        <stop
-          offset={0.158}
-          style={{
-            stopColor: "#672f90",
-          }}
-        />
-        <stop
-          offset={0.261}
-          style={{
-            stopColor: "#6c3793",
-          }}
-        />
-        <stop
-          offset={0.348}
-          style={{
-            stopColor: "#764697",
-          }}
-        />
-        <stop
-          offset={0.426}
-          style={{
-            stopColor: "#845b9e",
-          }}
-        />
-        <stop
-          offset={0.498}
-          style={{
-            stopColor: "#9676a6",
-          }}
-        />
-        <stop
-          offset={0.5}
-          style={{
-            stopColor: "#9677a6",
-          }}
-        />
-        <stop
-          offset={0.515}
-          style={{
-            stopColor: "#937eaa",
-          }}
-        />
-        <stop
-          offset={0.589}
-          style={{
-            stopColor: "#869abc",
-          }}
-        />
-        <stop
-          offset={0.669}
-          style={{
-            stopColor: "#7cb0ca",
-          }}
-        />
-        <stop
-          offset={0.756}
-          style={{
-            stopColor: "#75bfd4",
-          }}
-        />
-        <stop
-          offset={0.856}
-          style={{
-            stopColor: "#70c8da",
-          }}
-        />
-        <stop
-          offset={1}
-          style={{
-            stopColor: "#6fcbdc",
-          }}
-        />
-      </linearGradient>
-      <path
-        style={{
-          fill: "url(#SVGID_00000029030604317342557740000017856680528956840323_)",
-        }}
-        d="M842.5 746.8c-51.8 0-93.8-42-93.8-93.8V370.9L602 514.2c-36.7 35.8-95.4 35.6-131.7-.6l-139-138.3V653c0 51.8-42 93.8-93.8 93.8s-93.8-42-93.8-93.8V149.6c0-37.9 22.8-72.1 57.8-86.6 35-14.6 75.3-6.6 102.2 20.1L537 315.4 777 81c27-26.4 67.2-34 102-19.3 34.8 14.6 57.4 48.7 57.4 86.5V653c-.1 51.8-42.1 93.8-93.9 93.8z"
-      />
-    </g>
+    <circle
+      cx="402.8"
+      cy="603"
+      r="49.1"
+      fill="url(#uuid-153606bb-c80f-4f34-af6c-307f968c667f)"
+      data-name="Circle"
+    ></circle>
+    <path
+      fill="url(#uuid-92cc01c7-3ab1-49ea-b490-1d0c82972238)"
+      d="M410.8 971.4h-27.6c-204.4 0-377-189.6-377-414.1 0-51.8 40.2-93.8 89.7-93.8s89.7 42 89.7 93.8c0 118.5 94.2 226.4 197.6 226.4h27.6c103.4 0 197.6-107.9 197.6-226.4 0-51.8 40.2-93.8 89.7-93.8s89.7 42 89.7 93.8c0 224.5-172.6 414.1-377 414.1z"
+      data-name="D"
+    ></path>
+    <path
+      fill="url(#uuid-5a6d7213-39e7-408a-85fa-4c7fbd27f654)"
+      d="M698.8 692.5c-51.8 0-93.8-42-93.8-93.8V316.6L458.3 459.9c-36.7 35.8-95.4 35.6-131.7-.6L187.6 321v277.7c0 51.8-42 93.8-93.8 93.8S0 650.5 0 598.7V95.3C0 57.4 22.8 23.2 57.8 8.7c35-14.6 75.3-6.6 102.2 20.1l233.3 232.3 240-234.4c27-26.4 67.2-34 102-19.3 34.8 14.6 57.4 48.7 57.4 86.5v504.8c-.1 51.8-42.1 93.8-93.9 93.8z"
+      data-name="M"
+    ></path>
   </svg>
 )
-
-
-      }
+ }
