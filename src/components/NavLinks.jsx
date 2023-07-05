d550caea82ee8ae3e53ec41064ac18c6c0b5cd95
Jonathan <jjames422@me.com>
2023-07-05 00:07:57 -0400

    update to links

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -6,15 +6,17 @@ export function NavLinks() {
   let [hoveredIndex, setHoveredIndex] = useState(null)
 
   return [
-    ['Features', '#features'],
-    ['Reviews', '#reviews'],
+    ['Products', '#products'],
+    ['Hosting', '#hosting'],
+    ['WordPress', '#wordpress'],
+    ['Design', '#design'],
     ['Pricing', '#pricing'],
-    ['FAQs', '#faqs'],
+    ['For Business', '#for-business'],
   ].map(([label, href], index) => (
     <Link
       key={label}
       href={href}
-      className="relative -mx-3 -my-2 rounded-lg px-3 py-2 text-sm text-violet-700 transition-colors delay-150 hover:text-white hover:delay-[0ms]"
+      className="relative -mx-3 -my-2 rounded-lg px-3 py-2 text-sm text-white transition-colors delay-150 hover:text-white hover:delay-[0ms]"
       onMouseEnter={() => setHoveredIndex(index)}
       onMouseLeave={() => setHoveredIndex(null)}
     >
